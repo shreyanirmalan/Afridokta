0ac414ec49b40790eec86becbcaf81c33c74cf22
Shreya <shreyanirmalan@gmail.com>
2018-03-12 13:08:21 -0400

    First commit

---
+++ Assets/scripts/Part.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part : MonoBehaviour {
+    public Text mytext;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick()
+    {
+        mytext.text = this.ToString();
+        mytext.text = mytext.text.Substring(0, mytext.text.Length - 6);
+
+    }
+}
